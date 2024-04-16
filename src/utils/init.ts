5de9852e8595c0bd514aafd88d97044fb07926f4
arangace <ahua291@aucklanduni.ac.nz>
2024-04-16 18:31:29 +1000

    changed initial character

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -2,16 +2,32 @@ import { KaboomCtx } from "kaboom";
 import { setCamScale } from "./set-camscale";
 
 export default function init(context: KaboomCtx) {
+  const characterSpriteOffset = 24;
   context.loadSprite("spritesheet", "./assets/spritesheet.png", {
     sliceX: 39,
     sliceY: 31,
     anims: {
-      "idle-down": 936,
-      "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
-      "idle-side": 975,
-      "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
-      "idle-up": 1014,
-      "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+      "idle-down": 936 + characterSpriteOffset,
+      "walk-down": {
+        from: 936 + characterSpriteOffset,
+        to: 939 + characterSpriteOffset,
+        loop: true,
+        speed: 8,
+      },
+      "idle-side": 975 + characterSpriteOffset,
+      "walk-side": {
+        from: 975 + characterSpriteOffset,
+        to: 978 + characterSpriteOffset,
+        loop: true,
+        speed: 8,
+      },
+      "idle-up": 1014 + characterSpriteOffset,
+      "walk-up": {
+        from: 1014 + characterSpriteOffset,
+        to: 1017 + characterSpriteOffset,
+        loop: true,
+        speed: 8,
+      },
     },
   });
 
